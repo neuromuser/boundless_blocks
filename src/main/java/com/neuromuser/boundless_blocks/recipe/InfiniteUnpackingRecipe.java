fb602695b148126b5c59b0b33488928df2717b01
Neuromuser <neuromuser@gmail.com>
2026-01-15 14:50:44 +0200

    Wrong texture for specific crafts #2 FIXED

--- src/main/java/com/neuromuser/boundless_blocks/recipe/InfiniteUnpackingRecipe.java
+++ src/main/java/com/neuromuser/boundless_blocks/recipe/InfiniteUnpackingRecipe.java
@@ -24,30 +24,64 @@ public class InfiniteUnpackingRecipe extends SpecialCraftingRecipe {
     @Override
     public boolean matches(RecipeInputInventory inventory, World world) {
         int infiniteItemCount = 0;
+        int totalItemCount = 0;
 
         for (int i = 0; i < inventory.size(); i++) {
             ItemStack stack = inventory.getStack(i);
             if (!stack.isEmpty()) {
-                if (InfiniteBlockItem.isInfiniteBlock(stack)) {
-                    infiniteItemCount++;
-                } else {
+                totalItemCount++;
+
+                if (world.isClient()) {
+                    // On client, Polymer disguises our item as the base block item
+                    // We need to check if it has our custom name format "∞ ... ∞"
+                    // or if it has the enchantment glint
+                    if (stack.hasGlint() && stack.hasCustomName()) {
+                        String name = stack.getName().getString();
+                        if (name.startsWith("∞") && name.endsWith("∞")) {
+                            infiniteItemCount++;
+                            continue;
+                        }
+                    }
+                    // If not our format, it's not an infinite item
                     return false;
+                } else {
+                    // Server side - check actual item type
+                    if (stack.getItem() instanceof InfiniteBlockItem && InfiniteBlockItem.isInfiniteBlock(stack)) {
+                        infiniteItemCount++;
+                    } else {
+                        return false;
+                    }
                 }
             }
         }
 
-        return infiniteItemCount == 1;
+        return infiniteItemCount == 1 && totalItemCount == 1;
     }
 
     @Override
     public ItemStack craft(RecipeInputInventory inventory, DynamicRegistryManager registryManager) {
         for (int i = 0; i < inventory.size(); i++) {
             ItemStack stack = inventory.getStack(i);
-            if (InfiniteBlockItem.isInfiniteBlock(stack)) {
-                net.minecraft.block.Block block = InfiniteBlockItem.getStoredBlock(stack);
-                if (block != null) {
-                    return new ItemStack(block.asItem(), 9);
+
+            net.minecraft.block.Block block = null;
+
+            if (stack.getItem() instanceof InfiniteBlockItem && InfiniteBlockItem.isInfiniteBlock(stack)) {
+                block = InfiniteBlockItem.getStoredBlock(stack);
+            }
+            else if (stack.hasNbt()) {
+                net.minecraft.nbt.NbtCompound nbt = stack.getNbt();
+                if (nbt != null && nbt.contains("StoredBlockId")) {
+                    String blockIdStr = nbt.getString("StoredBlockId");
+                    net.minecraft.util.Identifier blockId = net.minecraft.util.Identifier.tryParse(blockIdStr);
+                    if (blockId != null) {
+                        block = net.minecraft.registry.Registries.BLOCK.get(blockId);
+                    }
                 }
+            }
+
+            if (block != null) {
+                // Return 9 of the base block
+                return new ItemStack(block.asItem(), 9);
             }
         }
         return ItemStack.EMPTY;
